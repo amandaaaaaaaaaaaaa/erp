2cb527ef0c1cf41127b0618f8e65e327173c281c
amandaaaaaaaaaaaaa <s10243479@connect.np.edu.sg>
2022-09-21 15:46:12 +0800

    2

--- api.py
+++ api.py
@@ -2,10 +2,10 @@ import requests, datetime, time, json
 import streamlit as st
 
 url = "http://datamall2.mytransport.sg/ltaodataservice/ERPRates"
-headers = {"AccountKey": "Mc7ua7LzS0qr7Odw4xEaug==",
-           "accept": "application/json"}
-#headers = {"AccountKey": st.secrets["LTA_APIKEY"],
+#headers = {"AccountKey": "",
 #           "accept": "application/json"}
+headers = {"AccountKey": st.secrets["LTA_APIKEY"],
+           "accept": "application/json"}
 response = requests.request(method="get", url=url, headers=headers)
 lta_data = response.json()
 #print(json.dumps(lta_data, indent=4))
